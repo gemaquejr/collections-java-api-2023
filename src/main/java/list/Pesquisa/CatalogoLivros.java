4e64e57cedc1eec33a9f5e45d797758e0eb78231
gemaquejr <gemaquejr@hotmail.com>
2023-10-08 01:10:48 -0300

    Resolvendo desafio OrdenacaoPessoa
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- src/main/java/list/Pesquisa/CatalogoLivros.java
+++ src/main/java/list/Pesquisa/CatalogoLivros.java
@@ -51,4 +51,17 @@ public class CatalogoLivros {
         }
         return livrosPorTitulo;
     }
+
+    public static void main(String[] args) {
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 1", 2022);
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 2", 2023);
+        catalogoLivros.adicionarLivro("Livro 2", "Autor 2", 2021);
+        catalogoLivros.adicionarLivro("Livro 3", "Autor 3", 2020);
+        catalogoLivros.adicionarLivro("Livro 4", "Autor 4", 2019);
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("Autor 2"));
+        System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2020, 2022));
+    }
 }
