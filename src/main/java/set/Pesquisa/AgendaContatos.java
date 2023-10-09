671cb66a67c5c74c7e3f45da8020776620809594
gemaquejr <gemaquejr@hotmail.com>
2023-10-09 20:00:14 -0300

    Resolvendo desafio CadastroProdutos imprimindo metodos de set
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

---
+++ src/main/java/set/Pesquisa/AgendaContatos.java
@@ -0,0 +1,61 @@
+package main.java.set.Pesquisa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+    //atributo
+    private Set<Contato> contatoSet;
+
+    public AgendaContatos() {
+        this.contatoSet = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int numero) {
+        contatoSet.add(new Contato(nome, numero));
+    }
+
+    public void exibirContatos() {
+        System.out.println(contatoSet);
+    }
+
+    public Set<Contato> pesquisarPorNome(String nome) {
+        Set<Contato> contatosPorNome = new HashSet<>();
+        for (Contato c : contatoSet) {
+            if (c.getNome().startsWith(nome)) {
+                contatosPorNome.add(c);
+            }
+        }
+        return contatosPorNome;
+    }
+
+    public Contato atualizarNumeroContato(String nome, int novoNumero) {
+        Contato contatoAtualizado = null;
+        for (Contato c : contatoSet) {
+            if (c.getNome().equalsIgnoreCase(nome)) {
+                c.setNumero(novoNumero);
+                contatoAtualizado = c;
+                break;
+            }
+        }
+        return contatoAtualizado;
+    }
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.exibirContatos();
+
+        agendaContatos.adicionarContato("Gemaque", 123456);
+        agendaContatos.adicionarContato("Gemaque", 0);
+        agendaContatos.adicionarContato("Gemaque Junior", 111111);
+        agendaContatos.adicionarContato("Gemaque Jr", 78910);
+        agendaContatos.adicionarContato("Edna Gemaque", 111111);
+
+        agendaContatos.exibirContatos();
+
+        System.out.println(agendaContatos.pesquisarPorNome("Gemaque"));
+
+        System.out.println("Contato atualizado: " + agendaContatos.atualizarNumeroContato("Gemaque Jr", 555555));
+    }
+}
